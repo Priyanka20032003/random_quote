a8358b0a0da2e07a4b5cee72eec153de2902e138
Priyanka20032003 <guntamadugupriyanka2003@gmail.com>
2024-08-12 13:35:21 +0530

    added media queries

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-function generate(){
-    var quotes={
-        " -- Jules Renard":'“The truly free man is the one who can turn down an invitation to dinner without giving an excuse.”',
-        "-- Albert Einstein":'“Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and its beauty.”',
-        "-- Pierce Brown" : '“Man cannot be freed by the same injustice that enslaved it.”'
-    }
+function generate() {
+  var quotes = {
+    "-- Maya Angelou":
+      "“You will face many defeats in life, but never let yourself be defeated.”",
+    "-- Oscar Wilde": "“Be yourself; everyone else is already taken.”",
+    "-- Nelson Mandela": "“It always seems impossible until it’s done.”",
+    "-- Confucius":
+      "“Our greatest glory is not in never falling, but in rising every time we fall.”",
+    "-- Steve Jobs":
+      "“Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.”",
+    "-- Mahatma Gandhi":
+      "“The best way to find yourself is to lose yourself in the service of others.”",
+    "-- Ralph Waldo Emerson":
+      "“What lies behind us and what lies before us are tiny matters compared to what lies within us.”",
+    "-- Albert Camus":
+      "“In the depth of winter, I finally learned that within me there lay an invincible summer.”",
+    "-- Eleanor Roosevelt":
+      "“The future belongs to those who believe in the beauty of their dreams.”",
+    "-- Winston Churchill":
+      "“Success is not final, failure is not fatal: It is the courage to continue that counts.”",
+  };
 
-    var authors = Object.keys(quotes);
-    console.log(authors)
+  var authors = Object.keys(quotes);
+  console.log(authors);
 
-    var author =authors[Math.floor(Math.random()*authors.length)]
-    console.log(author)
-    
+  var author = authors[Math.floor(Math.random() * authors.length)];
+  console.log(author);
 
-    var quote = quotes[author]
+  var quote = quotes[author];
 
-    document.getElementById("quote").innerHTML = quote;
-    document.getElementById("author").innerHTML = author;
+  document.getElementById("quote").innerHTML = quote;
+  document.getElementById("author").innerHTML = author;
 }
